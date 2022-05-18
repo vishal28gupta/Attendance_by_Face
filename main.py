21f55ff3a93271d7d3c1f5e1b07802f71c7ae18f
Subhadip De <88263132+subhadipde@users.noreply.github.com>
2022-05-18 12:32:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ class Face_Recognition_System:
         title_lbl=Label(bg_img, text="FACE RECOGNITION ATTENDANCE SYSTEM SOFTWARE", font=("times new roman", 35, "bold"), bg="white", fg="red")
         title_lbl.place(x=0, y=0,width=1530,height=45)
 
-        # student button
+        # Student button
         img4=Image.open(r"D:\Face_Attendance_Clg\images\Subha.jpg")
         img4=img4.resize((220,220), Image.ANTIALIAS)
         self.photoimg4=ImageTk.PhotoImage(img4)
@@ -27,8 +27,63 @@ class Face_Recognition_System:
         b1=Button(bg_img, image=self.photoimg4, cursor="hand2")
         b1.place(x=200,y=100,width=220,height=220)
 
-        b1_1=Button (bg_img, text="Face Detector", cursor="hand2", font=("times new roman", 15, "bold"), bg="darkblue", fg="white")
+        b1_1=Button (bg_img, text="Student Details", cursor="hand2", font=("times new roman", 15, "bold"), bg="darkblue", fg="white")
         b1_1.place(x=200, y=300,width=220,height=40)
+
+        # Face Recognition button
+        img4=Image.open(r"D:\Face_Attendance_Clg\images\Subha.jpg")
+        img4=img4.resize((220,220), Image.ANTIALIAS)
+        self.photoimg4=ImageTk.PhotoImage(img4)
+
+        b1=Button(bg_img, image=self.photoimg4, cursor="hand2")
+        b1.place(x=650,y=100,width=220,height=220)
+
+        b1_1=Button (bg_img, text="Face Detector", cursor="hand2", font=("times new roman", 15, "bold"), bg="darkblue", fg="white")
+        b1_1.place(x=650, y=300,width=220,height=40)
+
+        # Attendance button
+        img4=Image.open(r"D:\Face_Attendance_Clg\images\Subha.jpg")
+        img4=img4.resize((220,220), Image.ANTIALIAS)
+        self.photoimg4=ImageTk.PhotoImage(img4)
+
+        b1=Button(bg_img, image=self.photoimg4, cursor="hand2")
+        b1.place(x=1100,y=100,width=220,height=220)
+
+        b1_1=Button (bg_img, text="Attendance", cursor="hand2", font=("times new roman", 15, "bold"), bg="darkblue", fg="white")
+        b1_1.place(x=1100, y=300,width=220,height=40)
+
+        # Train Data button
+        img4=Image.open(r"D:\Face_Attendance_Clg\images\Subha.jpg")
+        img4=img4.resize((220,220), Image.ANTIALIAS)
+        self.photoimg4=ImageTk.PhotoImage(img4)
+
+        b1=Button(bg_img, image=self.photoimg4, cursor="hand2")
+        b1.place(x=200,y=400,width=220,height=220)
+
+        b1_1=Button (bg_img, text="Train Data", cursor="hand2", font=("times new roman", 15, "bold"), bg="darkblue", fg="white")
+        b1_1.place(x=200, y=600,width=220,height=40)
+
+        # Photos button
+        img4=Image.open(r"D:\Face_Attendance_Clg\images\Subha.jpg")
+        img4=img4.resize((220,220), Image.ANTIALIAS)
+        self.photoimg4=ImageTk.PhotoImage(img4)
+
+        b1=Button(bg_img, image=self.photoimg4, cursor="hand2")
+        b1.place(x=650,y=400,width=220,height=220)
+
+        b1_1=Button (bg_img, text="Photos", cursor="hand2", font=("times new roman", 15, "bold"), bg="darkblue", fg="white")
+        b1_1.place(x=650, y=600,width=220,height=40)
+
+        # Exit button
+        img4=Image.open(r"D:\Face_Attendance_Clg\images\Subha.jpg")
+        img4=img4.resize((220,220), Image.ANTIALIAS)
+        self.photoimg4=ImageTk.PhotoImage(img4)
+
+        b1=Button(bg_img, image=self.photoimg4, cursor="hand2")
+        b1.place(x=1100,y=400,width=220,height=220)
+
+        b1_1=Button (bg_img, text="Exit", cursor="hand2", font=("times new roman", 15, "bold"), bg="darkblue", fg="white")
+        b1_1.place(x=1100, y=600,width=220,height=40)
 
 
 if __name__ == "__main__":
